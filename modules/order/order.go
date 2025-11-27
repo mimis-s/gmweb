928cd9c28ac69b4bec3124a1e0d89ddd78ecf9e0
zhangbin04 <zhangbin04@dobest.com>
2025-11-27 18:28:27 +0800

    gm命令新增/删除/修改功能实现

--- modules/order/order.go
+++ modules/order/order.go
@@ -10,7 +10,7 @@ import (
 
 func GetGmOrderBoxReq(ctx *web.WebContext, roleId int64, req *webmodel.GetGmOrderBoxReq, rsp *webmodel.GetGmOrderBoxRsp) error {
 	rsp.ProjectId = req.ProjectId
-	rsp.Datas = make([]*webmodel.GmOrder, 0)
+	rsp.Datas = make([]*webmodel.RoleGmOrder, 0)
 
 	// 获取玩家数据, 里面有玩家拥有的权限组, 玩家使用命令的记录(点赞等)
 	roleDBData, find, err := dao.GetRoleData(roleId)
@@ -61,31 +61,21 @@ func GetGmOrderBoxReq(ctx *web.WebContext, roleId int64, req *webmodel.GetGmOrde
 		if data.Data == nil {
 			continue
 		}
-		isLike := false     // 当前玩家是否点赞
-		isBelittle := false // 当前玩家是否倒赞
-		iscollect := false  // 当前玩家是否收藏
 		lastRunArgs := ""
 		if roleDBData.OrderStatus != nil {
 			if len(roleDBData.OrderStatus.LastRunArgsMap) != 0 {
 				lastRunArgs = roleDBData.OrderStatus.LastRunArgsMap[data.Id]
 			}
-			if roleDBData.OrderStatus.OrderStatusMap != nil && roleDBData.OrderStatus.OrderStatusMap[data.Id] != nil {
-				isLike = roleDBData.OrderStatus.OrderStatusMap[data.Id][1]
-				isBelittle = roleDBData.OrderStatus.OrderStatusMap[data.Id][2]
-				iscollect = roleDBData.OrderStatus.OrderStatusMap[data.Id][3]
-			}
 		}
-		rsp.Datas = append(rsp.Datas, &webmodel.GmOrder{
-			OrderId:     data.Id,
-			OrderName:   data.Name,
-			OrderDesc:   data.Data.Desc,
-			Level:       data.Level,
-			OrderStruct: data.Data.OrderStruct,
+		rsp.Datas = append(rsp.Datas, &webmodel.RoleGmOrder{
+			GmOrderData: &webmodel.GmOrder{
+				OrderId:     data.Id,
+				OrderName:   data.Name,
+				OrderDesc:   data.Data.Desc,
+				Level:       data.Level,
+				OrderStruct: data.Data.OrderStruct,
+			},
 			LastRunArgs: lastRunArgs,
-			OrderStatus: data.Data.OrderStatusNum,
-			IsLike:      isLike,
-			IsBelittle:  isBelittle,
-			Iscollect:   iscollect,
 		})
 	}
 	return nil
